9812743ee7adc809ee3d399b29351d8df1bf318d
_hippo <elhaiba.hamza17@gmail.com>
2026-06-26 05:42:28 +0100

    style: add ambient glow spotlights to background on master

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`macchiato ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col relative overflow-x-hidden">
+        {/* Ambient Glow / Spotlights */}
+        <div className="fixed top-[-10%] right-[-5%] -z-10 w-[500px] h-[500px] rounded-full bg-ctp-mauve opacity-[0.15] blur-[120px] pointer-events-none"></div>
+        <div className="fixed bottom-[-10%] left-[-10%] -z-10 w-[600px] h-[600px] rounded-full bg-ctp-blue opacity-[0.10] blur-[150px] pointer-events-none"></div>
+        
         <Nav />
         <div className="max-w-xl mx-auto w-full px-6 flex flex-col flex-grow">
           <main className="flex-grow pb-16">{children}</main>
