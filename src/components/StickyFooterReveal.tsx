c29ecd223fe4f3458bb52973f7fc45e0e477b7fe
_hippo <elhaiba.hamza17@gmail.com>
2026-08-01 02:24:35 +0100

    fix: restore pointer events to footer links by removing negative z-index

--- src/components/StickyFooterReveal.tsx
+++ src/components/StickyFooterReveal.tsx
@@ -27,7 +27,7 @@ export default function StickyFooterReveal({ children }: { children: React.React
       <div id="footer-spacer" style={{ height }} className="w-full" />
       
       {/* The actual footer content, fixed at the bottom of the viewport, behind main content */}
-      <div className="fixed bottom-0 left-0 w-full z-[-1] pointer-events-none">
+      <div className="fixed bottom-0 left-0 w-full z-0 pointer-events-none">
         <div 
           ref={ref} 
           className="absolute bottom-0 left-0 w-full pointer-events-auto"
